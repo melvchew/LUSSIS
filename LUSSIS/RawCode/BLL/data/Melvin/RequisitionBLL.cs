d6d2759f9d855c7bb07148678bac2e799af15d3a
melvchew <melvchew@gmail.com>
2017-07-29 13:06:18 +0800

    Added folders and files

---
+++ LUSSIS/RawCode/BLL/data/Melvin/RequisitionBLL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUSSIS.RawCode.BLL.data.Melvin
+{
+    public class RequisitionBLL
+    {
+    }
+}
