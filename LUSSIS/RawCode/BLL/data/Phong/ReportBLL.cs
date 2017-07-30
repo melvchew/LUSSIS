7f1e703f36fc64dbe5d3396a07d0ead991b0ccc9
Phong Pham <lephongpham@gmail.com>
2017-07-30 18:21:19 +0800

    Phong .cs files

---
+++ LUSSIS/RawCode/BLL/data/Phong/ReportBLL.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUSSIS.RawCode.BLL.data.Phong
+{
+    public class ReportBLL
+    {
+        LUSSdbEntities context;
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataTable dt;
+        public List<Department> getDepartmentList()
+        {
+            context = new LUSSdbEntities();
+            List<Department> deptList;
+            deptList = context.Departments.ToList<Department>();
+            return deptList;
+
+        }
+        public List<Item> getItemList()
+        {
+            context = new LUSSdbEntities();
+            List<Item> itemList;
+            itemList = context.Items.ToList<Item>();
+            return itemList;
+
+        }
+
+        public List<String> getSubmitMonthList()
+        {
+            context = new LUSSdbEntities();
+            List<String> monthList = context.TransposedRequisitionReports.Select(x => x.SubmitMonth).Distinct().ToList();
+            return monthList;
+
+        }
+
+        public DateTime convertFromDate(string from)
+        {
+            DateTime fromDate;
+            int year = Int32.Parse(from.Substring(3, 4));
+            int month = Int32.Parse(from.Substring(0, 2));
+            fromDate = new DateTime(year, month, 01);
+
+            return fromDate;
+
+        }
+
+        public DateTime convertToDate(string to)
+        {
+
+            DateTime toDate;
+            int year = Int32.Parse(to.Substring(3, 4));
+            int month = Int32.Parse(to.Substring(0, 2));
+            toDate = new DateTime(year, month, DateTime.DaysInMonth(year, month)).AddDays(1);
+
+            return toDate;
+        }
+
+        public void CreateChartByQuantity(string itemId, string from, string to)
+        {
+            string query = "SELECT distinct ItemID, SubmitMonth, sum([1]) as [1], sum([2]) as [2], sum([3]) as [3]"
+                    + ",  sum([4]) as [4], sum([5])as [5], sum([6]) as [6],  sum([7]) as [7], sum([8]) as [8], sum([9]) as [9], sum([10]) as [10]"
+                    + " FROM TransposedRequisitionReport where ItemID=" + itemId
+                   + " and SubmitDate between '" + from
+                   + "' and '" + to
+                   + "' group by month(SubmitDate), year(SubmitDate), ItemID, SubmitMonth";
+
+            conn = new SqlConnection("Data Source=(local);Initial Catalog=LUSSdb;Integrated Security=True");
+            cmd = new SqlCommand(query, conn);
+
+            da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            dt = new DataTable();
+            da.Fill(dt);
+
+
+        }
+    }
+}
---
+++ LUSSIS/RawCode/BLL/data/Phong/RequisitionBLL.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUSSIS.RawCode.BLL.data.Phong
+{
+    public class RequisitionBLL
+    {
+        LUSSdbEntities context;
+        enum ReqStatus
+        {
+            PENDING = 1, CANCELLED, APPROVED, REJECTED, PARTIAL, DELIVERED
+        }
+
+        public Object GetRequisitionList()
+        {
+            context = new LUSSdbEntities();
+
+            //int i = (int)ReqStatus.APPROVED;
+            var q2 = (from ri in context.RequisitionItems
+                      where
+                        ri.Requisition.Status == "APPROVED"
+                      group new { ri.Item, ri } by new
+                      {
+                          ri.Item.Description,
+                          ri.Item.BinNumber,
+                          ri.Item.Category,
+                          ri.Item.Unit
+                      } into g
+                      select new
+                      {
+                          g.Key.BinNumber,
+                          g.Key.Category,
+                          g.Key.Description,
+                          Total_Requested = (int)g.Sum(p => p.ri.Quantity),
+                          g.Key.Unit
+                      }).ToList();
+
+            return q2;
+
+            //foreach (var ri in q2)
+            //{
+            //    Console.WriteLine("{0}, {1}, {2}, {3}, {4}",
+            //        ri.BinNumber, ri.Category, ri.Description, ri.Unit, ri.Total_Requested);
+
+            //}
+
+            //var q = items.GroupBy(f => f.Item.Description).Select(g => new { TotalQty = g.Sum(x => x.Quantity) }).ToList();
+            //foreach (var s in q)
+            //{
+            //    Console.WriteLine(s.TotalQty);
+            //}
+
+            //Console.ReadLine();
+        }
+
+        public Object GetRequisitionListByDepartment(int deptID)
+        {
+            LUSSdbEntities context = new LUSSdbEntities();
+
+            if (deptID == 0)
+            {
+                var all = (from ri in context.RequisitionItems
+                           where
+                             ri.Requisition.Status == "APPROVED"
+                           group new { ri.Item, ri } by new
+                           {
+                               ri.Item.Description,
+                               ri.Item.BinNumber,
+                               ri.Item.Category,
+                               ri.Item.Unit
+                           } into g
+                           select new
+                           {
+                               g.Key.BinNumber,
+                               g.Key.Category,
+                               g.Key.Description,
+                               Total_Requested = (int)g.Sum(p => p.ri.Quantity),
+                               g.Key.Unit
+                           }).ToList();
+
+                return all;
+            }
+            else
+            {
+                var query = (from ri in context.RequisitionItems
+
+                             where
+                               ri.Requisition.Status == "APPROVED" &&
+                               ri.Requisition.Employee.Department.DeptId == deptID
+                             group new { ri.Item, ri.Requisition.Employee.Department, ri } by new
+                             {
+                                 ri.Item.Description,
+                                 ri.Item.BinNumber,
+                                 ri.Item.Category,
+                                 ri.Requisition.Employee.Department.DeptName,
+                                 ri.Item.Unit
+                             } into g
+                             select new
+                             {
+                                 g.Key.BinNumber,
+                                 g.Key.Category,
+                                 Item = g.Key.Description,
+                                 Total_Requested = (int)g.Sum(p => p.ri.Quantity),
+                                 g.Key.Unit
+                             }).ToList();
+
+                return query;
+            }
+
+        }
+    }
+}
