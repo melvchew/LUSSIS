624992e2d52431fd2e813b8b020f019abe40fba6
melvchew <melvchew@gmail.com>
2017-07-30 13:25:32 +0800

    added Melvin's WebForms

---
+++ LUSSIS/Employee/DeptHead/ViewPendingReq.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewPendingReq.aspx.cs" Inherits="LUSSIS.Employee.DeptHead.ViewPendingReq" %>
+
+<!DOCTYPE html>
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+
+    <form id="form1" runat="server">
+        <div class="container-fluid">
+            <div>
+                <div class="col-lg-12">
+                    <asp:Label ID="Label_PageTitle" runat="server" Text="Label"></asp:Label>
+                </div>
+
+                <div class="col-lg-12">
+                    <asp:GridView ID="GridView_VPR" runat="server" AutoGenerateColumns="False" OnSelectedIndexChanged="GridView1_SelectedIndexChanged1">
+                        <Columns>
+
+                            <asp:BoundField DataField="ReqId" HeaderText="Requisition ID" />
+
+                            <asp:BoundField DataField="SubmitDate" HeaderText="Date" />
+
+                            <asp:BoundField DataField="Name" HeaderText="Employee Name" />
+
+
+                            <asp:TemplateField ShowHeader="False">
+                                <ItemTemplate>
+                                    <asp:LinkButton ID="LB_ViewDetail" runat="server" CausesValidation="False" CommandName="Select" Text="View Detail"></asp:LinkButton>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+
+                        </Columns>
+
+                    </asp:GridView>
+
+                </div>
+
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ LUSSIS/Employee/DeptHead/ViewPendingReq.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LUSSIS.Employee.DeptHead
+{
+    public partial class ViewPendingReq : System.Web.UI.Page
+    {
+        Service s = new Service();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label_PageTitle.Text = "Pending Requisitions";
+
+            try
+            {
+                //Get User Data from Session, Create User object
+                //Employee head = s.GetEmpByID(SessionParam)
+                //Create Dept object using User object
+                //Department dept = s.GetDeptByUser(head);
+                Department dept = s.GetDeptByID(1); //hardcoded
+                //Department dept = s.GetDeptByID(Session.Param)
+
+                //Get List of employees
+                List<Employee> Lemp = s.GetEmployees();
+
+                //Get Pending requisitions based on department
+                List<Requisition> Lreq = s.GetPendingReqByDepartment(dept);
+
+                //Bind Data from Lreq to Grid View
+                GridView_VPR.DataSource = Lreq.Select(o => new { ReqId = o.ReqId, SubmitDate = o.SubmitDate, Name = Lemp.FirstOrDefault(x => x.EmpId == o.EmpId).Name });
+                GridView_VPR.DataBind();
+            }
+            catch
+            {
+
+            }
+
+        }
+
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Direct to pendingdetail page including the ReqID selected
+            String value = GridView_VPR.Rows[GridView_VPR.SelectedIndex].Cells[0].Text;
+            Response.Redirect("PendingRequisition.aspx?ReqId=" + value); //pass the value to the next page.
+
+        }
+
+        protected void GridView1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+            //Direct to pendingdetail page including the ReqID selected
+            String value = GridView_VPR.Rows[GridView_VPR.SelectedIndex].Cells[0].Text;
+            Response.Redirect("PendingRequisition.aspx?ReqId=" + value); //pass the value to the next page.
+
+        }
+    }
+}
