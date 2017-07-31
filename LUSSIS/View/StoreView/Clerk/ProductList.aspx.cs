97d3b144b07762e4d0c7cd2c84f8b9ce7c145a91
Admin <Admin@USER>
2017-07-31 10:32:40 +0800

    [add]

---
+++ LUSSIS/View/StoreView/Clerk/ProductList.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LUSSIS.View.StoreView.Clerk
+{
+    public partial class ProductList : System.Web.UI.Page
+    {
+        LUSSdbEntities context;
+        BissLog bl = new BissLog();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                this.BindGrid();
+            }
+        }
+
+        private void BindGrid()
+        {
+            using (context = new LUSSdbEntities())
+            {
+
+                gvProductList.DataSource = bl.searchProductList(txtSearch.Text);
+                gvProductList.DataBind();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AddNewProduct.aspx?username=");
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("UpdateProduct.aspx?id=");
+        }
+
+        protected void gvProductList_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            String id = gvProductList.Rows[e.NewEditIndex].Cells[0].Text;
+            Response.Redirect("UpdateProduct.aspx?id=" + id);
+
+        }
+
+        protected void gvProductList_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvProductList.PageIndex = e.NewPageIndex;
+            BindGrid();
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+    }
+}
---
+++ LUSSIS/View/StoreView/Supervisor/AdjVoucherBelow250.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LUSSIS.View.StoreView.Supervisor
+{
+    public partial class AdjVoucherBelow250 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["message"] != null)
+                {
+                    lblAppOrRej.Text = Request.QueryString["message"];
+                }
+                else
+                {
+                    lblAppOrRej.Text = null;
+                }
+                BissLog b = new BissLog();
+                gvVoucher.DataSource = b.getAdjVoucherListBelow250();
+                gvVoucher.DataBind();
+            }
+        }
+
+        protected void gvVoucher_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            String id = gvVoucher.Rows[e.NewEditIndex].Cells[0].Text;
+            Response.Redirect("AdjustmentVoucherDetailBelow250.aspx?id=" + id);
+        }
+    }
+}
