44ff12c532656fcf6b618340d1816c58f77b9deb
Cornelia0227 <E0167315@u.nus.edu>
2017-07-31 14:11:12 +0800

    change the aspx name in .cs code

--- LUSSIS/View/DepartmentView/Emp/ViewReq.aspx.cs
+++ LUSSIS/View/DepartmentView/Emp/ViewReq.aspx.cs
@@ -43,7 +43,7 @@ namespace LUSSIS.View.DepartmentView.Emp
             }
             else if ((String)Session["View"] == "own")
             {
-                Response.Redirect("ViewOwnReqHistory.aspx");
+                Response.Redirect("ViewUserReqHistory.aspx");
             }
         }
     }
