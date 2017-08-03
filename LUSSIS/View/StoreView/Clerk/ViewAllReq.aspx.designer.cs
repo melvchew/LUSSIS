69c77c0e9a239b7f5d9401f45cedfc9186db05aa
Phong Pham <lephongpham@gmail.com>
2017-08-03 09:44:05 +0800

    a

--- LUSSIS/View/StoreView/Clerk/ViewAllReq.aspx.designer.cs
+++ LUSSIS/View/StoreView/Clerk/ViewAllReq.aspx.designer.cs
@@ -22,22 +22,22 @@ namespace LUSSIS.View.StoreView.Clerk {
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
         
         /// <summary>
-        /// Label3 control.
+        /// Label1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
+        protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
-        /// Label4 control.
+        /// Label2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label4;
+        protected global::System.Web.UI.WebControls.Label Label2;
         
         /// <summary>
         /// GridView1 control.
